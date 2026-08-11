e23a2e808118a15bbeaa3c0dbb571b739300dd03
Faiza Tehreem <faizatehreem786@gmail.com>
2026-08-11 12:47:18 +0500

    chore: added URLs constans

--- web/config/settings.py
+++ web/config/settings.py
@@ -111,3 +111,6 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+LOGIN_URL = "accounts:login"
+LOGIN_REDIRECT_URL = "dashboard:home"
+LOGOUT_REDIRECT_URL = "accounts:login"
